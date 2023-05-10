b14821e918a938f126851034252dd57acafc176a
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-10 08:25:36 +0600

    slider image and text designed

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,135 @@
+import { Link } from 'react-router-dom'
+import img1 from '../../../assets/images/banner/1.jpg'
+import img2 from '../../../assets/images/banner/2.jpg'
+import img3 from '../../../assets/images/banner/3.jpg'
+import img4 from '../../../assets/images/banner/4.jpg'
+import img5 from '../../../assets/images/banner/5.jpg'
+import img6 from '../../../assets/images/banner/6.jpg'
+
+
+const Banner = () => {
+    return (
+        <div className="carousel w-full h-[70vh]">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={img1} className="w-full rounded-xl" />
+                <div className="absolute flex items-center pl-12 text-white left-0 w-1/2 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full top-0">
+                    <div className='space-y-7'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <Link className='btn btn-outline btn-secondary mr-5'>Discover More</Link>
+                            <Link className='btn btn-outline btn-secondary'>Latest Project</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={img2} className="w-full rounded-xl" />
+                <div className="absolute flex items-center pl-12 text-white left-0 w-1/2 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full top-0">
+                    <div className='space-y-7'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <Link className='btn btn-outline btn-secondary mr-5'>Discover More</Link>
+                            <Link className='btn btn-outline btn-secondary'>Latest Project</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide1" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={img3} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center pl-12 text-white left-0 w-1/2 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full top-0">
+                    <div className='space-y-7'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <Link className='btn btn-outline btn-secondary mr-5'>Discover More</Link>
+                            <Link className='btn btn-outline btn-secondary'>Latest Project</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide2" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={img4} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center pl-12 text-white left-0 w-1/2 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full top-0">
+                    <div className='space-y-7'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <Link className='btn btn-outline btn-secondary mr-5'>Discover More</Link>
+                            <Link className='btn btn-outline btn-secondary'>Latest Project</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide3" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide5" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide5" className="carousel-item relative w-full">
+                <img src={img5} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center pl-12 text-white left-0 w-1/2 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full top-0">
+                    <div className='space-y-7'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <Link className='btn btn-outline btn-secondary mr-5'>Discover More</Link>
+                            <Link className='btn btn-outline btn-secondary'>Latest Project</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide4" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide6" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide6" className="carousel-item relative w-full">
+                <img src={img6} className="w-full rounded-xl" />
+
+                <div className="absolute flex items-center pl-12 text-white left-0 w-1/2 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0)] h-full top-0">
+                    <div className='space-y-7'>
+                        <h2 className="text-6xl font-bold">Affordable Price For Car Servicing</h2>
+                        <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
+                        <div>
+                            <Link className='btn btn-outline btn-secondary mr-5'>Discover More</Link>
+                            <Link className='btn btn-outline btn-secondary'>Latest Project</Link>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div className="absolute flex justify-end transform -translate-y-1/2 left-5 right-5 bottom-0">
+                    <a href="#slide5" className="btn btn-circle mr-5">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
