775faa49285760870aa4517d7ea0dbc454250960
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-10 11:10:21 +0600

    firebase config added

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,48 @@
+import img from '../../assets/images/login/login.svg'
+
+const Login = () => {
+
+
+    const handleLogin = event =>{
+        event.preventDefault();
+        
+
+    }
+    return (
+        <div className="hero min-h-screen my-10 bg-base-200">
+            <div className="hero-content flex-col lg:flex-row">
+                <div>
+
+                    <img src={img} alt="" />
+                </div>
+                <div className="card w-full lg:w-1/2  shadow-2xl bg-base-100">
+                    <div className="card-body">
+                        <h1 className="text-3xl text-center font-bold">Login</h1>
+                        <form onSubmit={handleLogin}>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Email</span>
+                                </label>
+                                <input type="text" placeholder="email" className="input input-bordered" />
+                            </div>
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Password</span>
+                                </label>
+                                <input type="text" placeholder="password" className="input input-bordered" />
+                                <label className="label">
+                                    <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                </label>
+                            </div>
+                            <div className="form-control mt-6">
+                                <input className="btn btn-primary" type="submit" value="Login" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
