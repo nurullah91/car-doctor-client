83a3bce14885e495ed73f6b55e2abaa697025ce6
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-12 10:59:17 +0600

    login log out button added conditionally

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom';
 import logo from '../../../assets/logo.svg';
+import { useContext } from 'react';
+import { AuthContext } from '../../../Providers/AuthProvider';
 
 const Navbar = () => {
+
+    const {user, logOut} = useContext(AuthContext);
+
+    const handleLogOut = () =>{
+        logOut()
+        .then( () =>{})
+        .catch(err =>console.log(err));
+    }
+
     const navItem = <>
     <li><Link>Home</Link></li>
     <li><Link>About</Link></li>
     <li><Link>Service</Link></li>
     <li><Link>Blog</Link></li>
     <li><Link>Contacts</Link></li>
+   { user?.email? <li><button onClick={handleLogOut}>Log out</button></li>:
+    <li><Link to='/login'>Login</Link></li>}
     
     </>
     return (
